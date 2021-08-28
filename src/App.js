ced924269221c24817d50e49ed2c1c91e11fefb6
Mickey English <mickeyenglish@mickeys-mbp.home>
2021-08-28 21:20:27 +0100

    Convert csv to json

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
+import { useEffect } from 'react'
+import { readString, CSVReader, readRemoteFile } from 'react-papaparse'
+import data from './data/phe_cases_london_boroughs.csv'
 import './App.css'
 
 function App() {
+  const loadData = () => {
+    readRemoteFile(data, {
+      header: true,
+      download: true,
+      complete: results => {
+        console.log(results.data)
+      }
+    })
+  }
+
+  useEffect(() => {
+    loadData()
+  }, [])
+
   return (
     <div className='App'>
       <h1>Hello world</h1>
